893b162561b9a293dd3bf640fc5762cd47c746fd
micahspencer-png <micahspencer@isu.edu>
2025-09-18 08:54:13 -0600

    Add project files.

---
+++ FortuneCookie/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FortuneCookie
+{
+    //Micah Spencer
+    //RCET2265
+    //Fall 2025
+    //Fortune Cookie Program
+    //
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
