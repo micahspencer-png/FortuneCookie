f42b91a0072acb7c75f34fd40e6c911bc9174fab
micahspencer-png <micahspencer@isu.edu>
2025-09-19 08:31:52 -0600

    Set up array. Still need to debug array

--- FortuneCookie/Program.cs
+++ FortuneCookie/Program.cs
@@ -10,8 +10,19 @@
     {
         static void Main(string[] args)
         {
+            string[] fortunes = [""] ;
+            
+            fortunes[0] = "Fortune 1";
+            fortunes[1] = "Fortune 2";
+            fortunes[2] = "Fortune 3";
+            fortunes[3] = "Fortune 4";
+            fortunes[4] = "Fortune 5";
+            fortunes[5] = "Fortune 6";
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(fortunes[0]);
+
+            Console.WriteLine("Press Enter to Quit");
+            Console.Read();
         }
     }
 }
