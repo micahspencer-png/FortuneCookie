7fad68e3915f54f9bd4fa4d4333e9f856b86fa55
micahspencer-png <micahspencer@isu.edu>
2025-09-21 20:24:53 -0600

    Set up fortunes in the array. Got the random number to work, and converted it so that the writeline function works.

--- FortuneCookie/Program.cs
+++ FortuneCookie/Program.cs
@@ -10,18 +10,30 @@
     {
         static void Main(string[] args)
         {
-            string[] fortunes = [""] ;
+            //sets up an array
+            string[] fortunes = ["0","1","2","3","4","5"] ;
             
-            fortunes[0] = "Fortune 1";
-            fortunes[1] = "Fortune 2";
-            fortunes[2] = "Fortune 3";
-            fortunes[3] = "Fortune 4";
-            fortunes[4] = "Fortune 5";
-            fortunes[5] = "Fortune 6";
+            //determines what is in the array
+            fortunes[0] = "There is No Wisdom Greater than Kindness.";
+            fortunes[1] = "Stay True to Your Values; They Define You.";
+            fortunes[2] = "You Discover Treasures Where Others See Nothing.";
+            fortunes[3] = "Your Potential Knows No Bounds.";
+            fortunes[4] = "Your Determination is Your Greatest Asset.";
+            fortunes[5] = "Good Friends are Worth More than Gold.";
 
-            Console.WriteLine(fortunes[0]);
+            //sets up a random number
+            Random Roll = new Random();
+            
+            //converts random number into an integer
+            int roll = Roll.Next(0, 6);
 
+            //writes the line that is determined by the number
+            Console.WriteLine(fortunes[roll]);
+            
+            //prompts the user to exit program
             Console.WriteLine("Press Enter to Quit");
+
+            //creates a pause
             Console.Read();
         }
     }
